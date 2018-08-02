d3b728cc7d77568c4a634dcd611fd2efd658eaa8
Frai <fraifelipe@gmail.com>
2018-08-01 22:41:20 -0300

    Init

---
+++ FluentApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FluentApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Voo.CriarVoo(1234).Origem("SP").Destinacao("RJ").Quando(DateTime.Today).Duracao(1);
+        }
+    }
+}
---
+++ FluentApi/Voo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Data;
+using System.Runtime.CompilerServices;
+
+namespace FluentApi
+{
+    public class Voo
+    {
+        string _origem;
+        string _destinacao;
+        DateTime _data;
+
+        public int VooId { get; private set; }
+
+        public Voo(int id)
+        {
+            this.VooId = id;
+        }
+        
+        public static Voo CriarVoo(int id)
+        {
+            return new Voo(id);
+        }
+
+        public Voo Origem(string origem)
+        {
+            this._origem = origem;
+            return this;
+        }
+
+        public Voo Destinacao(string destinacao)
+        {
+            this._destinacao = destinacao;
+            return this;
+        }
+
+        public Voo Quando(DateTime data)
+        {
+            this._data = data;
+            return this;
+        }
+
+        public int Duracao(int horas)
+        {
+            return horas;
+        }
+    }
+}
